d4cb3c05e334f9005776a10316f25d54e124a5f3
Lucasegm <lucas.egm@hotmail.com>
2023-09-22 15:44:30 -0300

    First Update

---
+++ Introducao a Programacao/Atividade 1/exemplo 1.py
@@ -0,0 +1,6 @@
+numero = 21
+for i in range(20):
+    numero = numero - 1
+    if (numero == 13):
+        continue
+    print(numero)
---
+++ Introducao a Programacao/Atividade 1/exemplo 3.py
@@ -0,0 +1,7 @@
+i = 20
+while True:
+    if i != 13:
+        print(i)
+    i -= 1
+    if i < 1:
+        break
