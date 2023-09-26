c8e7ea2a46f125c00df87f5303e2b9e8e168d865
Lucasegm <lucas.egm@hotmail.com>
2023-09-26 12:18:33 -0300

    Update Atividade 1

--- Preparação para HTML e CSS/Exercicios/Atividade 1/Atividade 1.py
+++ Preparação para HTML e CSS/Exercicios/Atividade 1/Atividade 1.py
@@ -1,6 +1,6 @@
 loja_de_informatica = ["Mouse", "Teclado", "Processador", "Placa de Vídeo", "Memória RAM"]
 nascimento_amigos = [1998, 1995, 1993, 1992, 1985]
 
-print("O produto mais adquirido em nossa loja é {}".format(loja_de_informatica[3]))
+print("Os itens mais vendidos de nossa loja são: {}".format(loja_de_informatica))
 # Lembrando que a posição do indice no array começa em 0(Ex: 0, 1, 2, 3)
-print("O meu amigo mais velho nasceu em {}".format(nascimento_amigos[4]))
+print("O ano de nascimento dos meus amigos e familiares são: {}".format(nascimento_amigos))
