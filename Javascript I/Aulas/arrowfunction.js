de3c715bdabb49c1605ffa46846bf3301c46f4ad
Lucasegm <lucas.egm@hotmail.com>
2023-11-08 21:07:27 -0300

    Atividade 4 - JS I

---
+++ Javascript I/Aulas/arrowfunction.js
@@ -0,0 +1,9 @@
+// Arrow function
+
+const proximoNum = (n) => {
+
+    return (n + 1)
+
+}
+
+console.log(proximoNum(4199))
---
+++ Javascript I/Exercicios/Atividade 4/script.js
@@ -0,0 +1,7 @@
+const imprimirMensagem = () => {
+    console.log("Conexão feita com sucesso!");
+  };
+  
+  for (let i = 0; i < 3; i++) {
+    imprimirMensagem();
+  }
